c35628948002b6f30335e982694788a680a3b607
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-22 11:09:50 -0700

    add second passsing test for checkRow function
    co-authored-by: Andrew Giang <giang184@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -3,7 +3,7 @@ import {Sudoku} from './../src/sudoku.js'
 describe('Sudoku', () => {
   
   test('Sudoku constructor should have a two dimmensional array', () => {
-    const s = new Sudoku() 
+    const s = new Sudoku(); 
     expect(s.board).toEqual([
       [0,0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0,0],
@@ -17,4 +17,11 @@ describe('Sudoku', () => {
       [0,0,0,0,0,0,0,0,0,0],
     ]);
   });
+
+  test('checkRow function should identify rows', () => {
+    const s = new Sudoku();
+    s.board[5] = [0,1,2,3,4,5,6,7,8,9];
+    expect(s.checkRow(5)).toEqual(true);
+  });
+  
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -13,16 +13,15 @@ export function Sudoku() {
   ];
 }
 
-
 Sudoku.prototype.checker = function () {
   Sudoku.checkRow();
   Sudoku.checkCol();
   Sudoku.checkSquare();
 }
 
-Sudoku.prototype.checkRow = function () {
+Sudoku.prototype.checkRow = function (rowNum) {
   for(let i=1; i<=9; i++) {
-    if(this.row1.indexOf(i) === -1)
+    if(this.board[rowNum].indexOf(i) === -1)
       return false;
   }
   return true;
