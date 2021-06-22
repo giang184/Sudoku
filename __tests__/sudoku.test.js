ca780da246218a7b4be799cf2d6e6ff6a66ceef9
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-22 11:28:13 -0700

    add third passing test and two additional tests to check for false
    co-authored-by: Andrew Giang <giang184@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -18,10 +18,45 @@ describe('Sudoku', () => {
     ]);
   });
 
-  test('checkRow function should identify rows', () => {
+  test('checkRow function should check rows correctly for occurence of numbers 1-9', () => {
     const s = new Sudoku();
     s.board[5] = [0,1,2,3,4,5,6,7,8,9];
     expect(s.checkRow(5)).toEqual(true);
   });
-  
+
+  test('checkRow function should check rows correctly for occurence of numbers 1-9', () => {
+    const s = new Sudoku();
+    s.board[5] = [0,1,1,3,4,5,6,7,8,9];
+    expect(s.checkRow(5)).toEqual(false);
+  });
+
+  test('checkCol function should check cols correctly for occurence of numbers 1-9', () => {
+    const s= new Sudoku();
+    s.board[0][3]=0;
+    s.board[1][3]=9;
+    s.board[2][3]=8;
+    s.board[3][3]=6;
+    s.board[4][3]=7;
+    s.board[5][3]=5;
+    s.board[6][3]=3;
+    s.board[7][3]=4;
+    s.board[8][3]=1;
+    s.board[9][3]=2;
+    expect(s.checkCol(3)).toEqual(true);
+  });
+
+  test('checkCol function should check cols correctly for occurence of numbers 1-9', () => {
+    const s= new Sudoku();
+    s.board[0][3]=0;
+    s.board[1][3]=2;
+    s.board[2][3]=8;
+    s.board[3][3]=6;
+    s.board[4][3]=7;
+    s.board[5][3]=5;
+    s.board[6][3]=3;
+    s.board[7][3]=4;
+    s.board[8][3]=1;
+    s.board[9][3]=2;
+    expect(s.checkCol(3)).toEqual(false);
+  });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -26,3 +26,17 @@ Sudoku.prototype.checkRow = function (rowNum) {
   }
   return true;
 }
+
+Sudoku.prototype.checkCol = function (colNum) {
+  let temp = [];
+  for(let i=0; i<=9; i++) {
+    temp.push(this.board[i][colNum]);
+  }
+
+  for(let i=1; i<=9; i++) {
+    if(temp.indexOf(i) === -1)
+      return false;
+  }
+
+  return true;
+}
